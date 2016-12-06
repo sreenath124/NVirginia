a283ba23f162bf7e369c01c12323f1d3fef52901
Sreenath P G <sreenathg@qburst.com>
2016-12-06 16:56:36 +0530

    Menu button functionality added

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+	$('#menu_button').click(function() {
+		$('#side_nav, #overlay').show('slow');
+		$('html ,body').css('overflow','hidden');
+	});
+	$('#close_button, #overlay').click(function(){
+		$('#side_nav, #overlay').hide('slow');
+		$('html, body').css('overflow', 'auto');
+	})
+})
