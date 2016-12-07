c623e9b2be0b6c379d29485872b20dbe631bf61b
Sreenath P G <sreenathg@qburst.com>
2016-12-07 14:42:55 +0530

    hamburger menu slide using transition

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-	$('#menu_button').click(function() {
-		$('#side_nav, #overlay').show('slow');
-		$('html ,body').css('overflow','hidden');
+	$('#menu_button').on('click',function(){
+		$('body').addClass('overlay_visible').removeClass('overlay_hidden');
 	});
-	$('#close_button, #overlay').click(function(){
-		$('#side_nav, #overlay').hide('slow');
-		$('html, body').css('overflow', 'auto');
+	$('#close_button, .overlay').on('click', function() {
+		$('body').removeClass('overlay_visible').addClass('overlay_hidden');
 	});
 });
