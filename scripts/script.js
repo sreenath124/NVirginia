c1714d5213a28db2296d82f39d85c8cee7e27a8e
Sreenath P G <sreenathg@qburst.com>
2016-12-07 15:11:16 +0530

    mixin for transition added

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-	$('#menu_button').on('click',function(){
-		$('body').addClass('overlay_visible').removeClass('overlay_hidden');
-	});
-	$('#close_button, .overlay').on('click', function() {
-		$('body').removeClass('overlay_visible').addClass('overlay_hidden');
-	});
+$(document).ready(function() {
+    $('#menu_button').on('click', function() {
+        $('body').addClass('overlay_visible').removeClass('overlay_hidden');
+    });
+    $('#close_button, .overlay').on('click', function() {
+        $('body').removeClass('overlay_visible').addClass('overlay_hidden');
+    });
 });
